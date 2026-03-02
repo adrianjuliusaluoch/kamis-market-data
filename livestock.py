08dafcb680e47503a39a6946a6b4a19ac8111584
adrianjuliusaluoch <adrianjuliusaluoch@gmail.com>
2026-03-02 19:47:23 +0000

    Auto update livestock.py timestamp [skip ci]

--- livestock.py
+++ livestock.py
@@ -1,4 +1,4 @@
-# Last run: Mon Mar  2 19:02:57 UTC 2026
+# Last run: Mon Mar  2 19:47:23 UTC 2026
 # Import Packages
 from google.cloud import bigquery
 from google.api_core.exceptions import NotFound
