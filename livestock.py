0ef82fc155b844cee47c8fd467a4bff8105257b4
adrianjuliusaluoch <adrianjuliusaluoch@gmail.com>
2026-04-25 21:47:19 +0000

    Keep workflow alive [skip ci]

--- livestock.py
+++ livestock.py
@@ -1,4 +1,4 @@
-# Last run: Sat Apr 25 10:01:40 UTC 2026
+# Last run: Sat Apr 25 21:47:19 UTC 2026
 # Import Packages
 from google.cloud import bigquery
 from google.api_core.exceptions import NotFound
